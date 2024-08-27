bdc03252c883407adfd9b9aa68cca0fdd871246c
Henrique Bertochi Grigol <henriquebg.bg@outlook.com>
2024-08-26 23:08:30 -0300

    remove pastes

---
+++ tabuada.py
@@ -0,0 +1,10 @@
+n = input('digite um numero: ')
+for c in range(1, 11):
+    print(f'{n} x {c} = {int(n)*c}')
+
+#
+
+n = input('digite um numero: ')
+c = 1
+while c <= 10:
+    print(f'{n} x {c} = {int(n)*c}')
